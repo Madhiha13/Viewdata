2650a586b1f370610f0bc67facc29c8ce6bec7fa
Madhiha13 <madhihakhanamsk13@gmail.com>
2024-04-29 12:08:20 +0530

    first commit

--- src/List/FinalList.jsx
+++ src/List/FinalList.jsx
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import "../List_css/FinalList.css"
+
 
 import {CartSvg,CircleSvg,WhitecartSvg,FilterSvg,Co2,FolderSvg,GreenSvg,LeftArrow,PiechartSvg,UserSvg,WhitevariationSvg } from "../assets";
 const FinalList= () => {
+  
+  
+
     const [dataArray, setDataArray] = useState([]);
      const [selectedYear, setSelectedYear] = useState(''); 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:5000/api/data');
-                const jsonData = await response.json();
-                setDataArray(jsonData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
 
-        fetchData();
-    }, []);
 
+     useEffect(() => {
+      // Initialize staticDataArray inside the useEffect callback
+      const staticDataArray = [
+          { id: 1, emissionType: 'Company Owned vehicles usage (Mobile combustion)', facilty: 'Facility1', reportingYear: '2022-2023', month: 'January', fuel: 'petrol', quantity: 14478, units: 'Litres', emission: 876, typeofvehicle: 'Forklift' },
+          { id: 2, emissionType: 'Company Owned vehicles usage (Mobile combustion)', facilty: 'Facility2', reportingYear: '2022-2023', month: 'February', fuel: 'diesel', quantity: 463, units: 'Litres', emission: 853, typeofvehicle: 'Forklift' },
+          { id: 3, emissionType: 'Company Owned vehicles usage (Mobile combustion)', facilty: 'Facility3', reportingYear: '2022-2023', month: 'March', fuel: 'petrol', quantity: 19767, units: 'Litres', emission: 254, typeofvehicle: 'Truck' },
+          { id: 4, emissionType: 'Company Owned vehicles usage (Mobile combustion)', facilty: 'Facility4', reportingYear: '2022-2023', month: 'January', fuel: 'diesel', quantity: 80, units: 'Litres', emission: 233, typeofvehicle: 'Car' },
+          { id: 5, emissionType: 'Company Owned vehicles usage (Mobile combustion)', facilty: 'Facility5', reportingYear: '2022-2023', month: 'January', fuel: 'diesel', quantity: 177, units: 'Litres', emission: 55, typeofvehicle: 'Forklift' }
+      ];
+
+      setDataArray(staticDataArray); // Set the static data array to the state
+  }, []); 
     const hasMoreUsers = dataArray.length > 3;
 
     const yearRanges = [
